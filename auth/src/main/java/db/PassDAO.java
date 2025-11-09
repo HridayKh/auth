cb42d6ab3ed8f8516bec29272a6d63027cd5e48a
HridayKh <hridaykh1234@gmail.com>
2025-11-09 19:27:49 +0530

    feat: Add email token management for user verification and implement related servlet methods

---
+++ auth/src/main/java/db/PassDAO.java
@@ -0,0 +1,62 @@
+package db;
+
+import entities.EmailToken;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PassDAO {
+
+	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
+	public static boolean insertEmailToken(Connection conn, EmailToken et) {
+		String sql = "INSERT INTO email_tokens (token, user_uuid, expires_at) VALUES (?, ?, ?)";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, et.token());
+			stmt.setString(2, et.user_uuid());
+			stmt.setLong(3, et.expires_at());
+			int rowsInserted = stmt.executeUpdate();
+			return rowsInserted > 0;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+
+	public static String userUserUuidFromVerifyToken(Connection conn, String token) {
+		String sql = "SELECT user_uuid FROM email_tokens WHERE token = ? AND expires_at > (UNIX_TIMESTAMP()) LIMIT 1";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, token);
+			ResultSet rs = stmt.executeQuery();
+			return rs.next() ? rs.getString("user_uuid") : null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static boolean expireToken(Connection conn, String token) {
+		String sql = "UPDATE email_tokens SET expires_at = (UNIX_TIMESTAMP() * 1000) WHERE token = ?";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, token);
+			int affectedRows = stmt.executeUpdate();
+			return affectedRows > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static boolean deleteEmailTokenByUser(Connection conn, String uuid) {
+		String sql = "DELETE FROM email_tokens WHERE user_uuid = ?";
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+			stmt.setString(1, uuid);
+			int affectedRows = stmt.executeUpdate();
+			return affectedRows > 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
---
+++ auth/src/main/java/servlets/userPasswords/UsersPassReset.java
@@ -0,0 +1,112 @@
+package servlets.userPasswords;
+
+import db.EmailDAO;
+import db.UsersDAO;
+import db.dbAuth;
+import entities.EmailToken;
+import entities.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+import servlets.ApiConstants;
+import utils.AuthUtil;
+import utils.HttpUtil;
+import utils.MailUtil;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+
+public class UsersPassReset {
+
+	private static final Logger log = LogManager.getLogger(UsersPassReset.class);
+
+	public static void verifyUser(HttpServletRequest req, HttpServletResponse resp, Map<String, String> ignoredParams) throws IOException {
+		String token = req.getParameter("token");
+		String redirect = req.getParameter("redirect");
+
+		if (token == null || token.isBlank()) {
+			resp.sendRedirect(dbAuth.FRONT_HOST + "/register?redirect=" + redirect + "&type=error&msg=Missing/Invalid Token");
+			return;
+		}
+
+		try (Connection conn = dbAuth.getConnection()) {
+			conn.setAutoCommit(false);
+
+			String userUuid = EmailDAO.userUserUuidFromVerifyToken(conn, token);
+			if (userUuid == null) {
+				conn.rollback();
+				resp.sendRedirect(dbAuth.FRONT_HOST + "/register?redirect=" + redirect + "&type=error&msg=Invalid or Expired email verification token");
+				return;
+			}
+
+			boolean userVerify = UsersDAO.updateUserVerifyStatus(conn, userUuid);
+			if (!userVerify) {
+				conn.rollback();
+				resp.sendRedirect(dbAuth.FRONT_HOST + "/register?redirect=" + redirect + "&type=error&msg=Unable to verify user");
+				return;
+			}
+
+			boolean expireToken = EmailDAO.expireToken(conn, token);
+			if (!expireToken) {
+				conn.rollback();
+				resp.sendRedirect(dbAuth.FRONT_HOST + "/register?redirect=" + redirect + "&type=error&msg=Unable to expire token");
+				return;
+			}
+
+			AuthUtil.createAndSetAuthCookie(conn, req, resp, userUuid);
+
+			conn.commit();
+			resp.sendRedirect(redirect + "?type=success&msg=Email verified successfully.");
+		} catch (SQLException e) {
+			log.catching(e);
+			resp.sendRedirect(dbAuth.FRONT_HOST + "/register?redirect=" + redirect + "&type=error&msg=Unexpected server error");
+		}
+	}
+
+	public static void resendVerifyEmail(HttpServletRequest req, HttpServletResponse resp, Map<String, String> ignoredParams) throws IOException {
+		JSONObject body = HttpUtil.readBodyJSON(req);
+		String email = body.getString("email").toLowerCase();
+		String redirectUrl = body.getString("redirect");
+
+		if (email == null || email.isBlank()) {
+			HttpUtil.sendJson(resp, HttpServletResponse.SC_BAD_REQUEST, "error", "Invalid Email Provided.");
+			return;
+		}
+
+		try (Connection conn = dbAuth.getConnection()) {
+			User user = UsersDAO.getUserByEmail(conn, email);
+			if (user == null) {
+				HttpUtil.sendJson(resp, HttpServletResponse.SC_NOT_FOUND, "error", "No such user found.");
+				return;
+			}
+			if (user.isVerified()) {
+				HttpUtil.sendJson(resp, HttpServletResponse.SC_BAD_REQUEST, "error", "User with given email is already verified.");
+				return;
+			}
+
+			String newToken = UUID.randomUUID().toString();
+
+			if (!EmailDAO.deleteEmailTokenByUser(conn, user.uuid())) {
+				HttpUtil.sendJson(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "error", "Unable to invalidate old token.");
+				return;
+			}
+			if (!EmailDAO.insertEmailToken(conn, new EmailToken(newToken, user.uuid(), (System.currentTimeMillis() / 1000L) + 86_400))) {
+				HttpUtil.sendJson(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "error", "Token insert failed.");
+				return;
+			}
+
+			String verifyLink = dbAuth.BACK_HOST + ApiConstants.USERS_VERIFY_EMAIL + "?token=" + newToken + "&redirect=" + redirectUrl;
+			MailUtil.sendMail(email, "Your new HridayKh.in email verification link", MailUtil.templateVerifyMail(verifyLink));
+			HttpUtil.sendJson(resp, HttpServletResponse.SC_OK, "success", "A new verification email has been sent.");
+		} catch (Exception e) {
+			log.catching(e);
+			HttpUtil.sendJson(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "error", "Server error occurred.");
+		}
+	}
+
+}
